60bf42622a56410f1b65a3278f0c3e886a882abf
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-02-10 11:29:21 +0200

    Update self_exam.py

--- self_exam.py
+++ self_exam.py
@@ -1,18 +1,21 @@
 import re
 import random
 from datetime import datetime
-
+import time
 
 file = open('quiz.txt', 'r')
 d1 = file.read()
 file.close()
 d2 = str(d1)
 print("Welcome user. Type 'done' whenever you want to quit.\n")
-time = datetime.now()
-date = time.strftime('%D')
-hour = time.strftime(':%H:%M:%S')
+time1 = datetime.now()
+date = time1.strftime('%D')
+hour = time1.strftime(':%H:%M:%S')
 global counter
 counter = 1
+open('wrong_answer.txt', 'w').close()
+open('correct_answers.txt', 'w').close()
+
 
 def split():
     print(f'Question {counter}/125')
@@ -23,22 +26,29 @@ def split():
     correct = x3[-1]
     print(question[:-9])
     answer = input('\nPlease enter your answer ').upper()
+
     def answer_check():
         global counter
         if answer == correct:
             print('Thats correct !\n')
-            right = open('correct_answers.txt','a')
-            right.write(date+" - "+hour+' Correct answer question number : '+str(page)+"\n")
+            right = open('correct_answers.txt', 'a')
+            right.write(date + " - " + hour + ' Correct answer question number : ' + str(page) + "\n")
             right.close()
             counter += 1
         elif answer == "done" or answer == "Done" or answer == "DONE":
-            print('See you later !')
+            answers1 = int(len(open('correct_answers.txt', 'r').readlines()))
+            percentage1 = (answers1 * counter) / 100
+            print(f'You have done {answers1} right answers out of {counter} questions.\nYour success rate is {percentage1}. See you next time !')
+            time.sleep(5)
+            open('wrong_answer.txt', 'w').close()
+            open('correct_answers.txt', 'w').close()
             quit()
         else:
-            print('Incorrect answer !\nThe correct answer is '+correct+'\n')
+            print('Incorrect answer !\nThe correct answer is ' + correct + '\n')
             wrong = open('wrong_answer.txt', 'a')
-            wrong.write(date+" - "+hour+' Incorrect answer question number : '+str(page)+"\n")
+            wrong.write(date + " - " + hour + ' Incorrect answer question number : ' + str(page) + "\n")
             counter += 1
+
     answer_check()
 
 
@@ -47,5 +57,9 @@ while counter <= 125:
 else:
     answers = int(len(open('correct_answers.txt', 'r').readlines()))
     wrongs = int(len(open('wrong_answer.txt', 'r').readlines()))
-    percentage = (answers*125)/100
-    print(f'You have finished to exam !\nYou have {answers} right answers and {wrongs} wrong answers.\n{percentage}% of succes.')
+    percentage = (answers * 125) / 100
+    print(f'You have finished to exam !\nYou have {answers} right answers and {wrongs} wrong answers.\n{percentage}% of success.')
+    time.sleep(5)
+    open('wrong_answer.txt', 'w').close()
+    open('correct_answers.txt', 'w').close()
+    quit()
