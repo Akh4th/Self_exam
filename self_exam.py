e7f114b2c6e04128cf56bfb273dfdaf6892fd2be
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-02-11 10:26:56 +0200

    Update self_exam.py

--- self_exam.py
+++ self_exam.py
@@ -15,6 +15,7 @@ global counter
 counter = 1
 open('wrong_answer.txt', 'w').close()
 open('correct_answers.txt', 'w').close()
+time.sleep(3)
 
 
 def split():
@@ -37,8 +38,8 @@ def split():
             counter += 1
         elif answer == "done" or answer == "Done" or answer == "DONE":
             answers1 = int(len(open('correct_answers.txt', 'r').readlines()))
-            percentage1 = ((answers1 * 100)/counter)
-            print(f'You have done {answers1} right answers out of {counter} questions.\nYour success rate is {percentage1}%. See you next time !')
+            percentage1 = ((answers1 * 100)/(counter-1))
+            print(f'You have done {answers1} right answers out of {counter-1} questions.\nYour success rate is {percentage1}%. See you next time !')
             time.sleep(5)
             open('wrong_answer.txt', 'w').close()
             open('correct_answers.txt', 'w').close()
