b26fd638ff28d4f4594974e2bdc67859eb4b40fd
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-02-14 20:56:50 +0200

    Update self_exam.py

--- self_exam.py
+++ self_exam.py
@@ -36,36 +36,31 @@ def split():
     def answer_check():
         global counter
         if answer == correct:
+            quest1 = int(page)
             ans_time = datetime.now()
             ans_date = ans_time.strftime('%D:')
             ans_hour = ans_time.strftime('%H:%M:%S')
             print('Thats correct !\n')
             right = open('correct_answers.txt', 'a')
-            right.write(ans_date + " - " + ans_hour + ' Correct answer question number : ' + str(page) + "\n")
+            right.write(ans_date + " - " + ans_hour + ' Correct answer question number : ' + quest1 + "\n")
             right.close()
             counter += 1
             clock()
             time.sleep(1)
         elif answer == "done" or answer == "Done" or answer == "DONE":
-            answers1 = int(len(open('correct_answers.txt', 'r').readlines()))
-            percentage1 = ((answers1 * 100) / (counter - 1))
-            done1 = input(f'You have done {answers1} right answers out of {counter - 1} questions.\nYour success rate is {percentage1}%.\nType YES in order to get score file. ').upper()
-            if done1 == 'YES':
-                print('results.txt has your wrong questions.')
-                score()
-            else:
-                quit()
+            done()
         elif answer == "skip" or answer == "Skip" or answer == "SKIP":
             clear()
             clock()
             return
         else:
+            quest1 = int(page)
             ans_time = datetime.now()
             ans_date = ans_time.strftime('%D:')
             ans_hour = ans_time.strftime('%H:%M:%S')
             print('Incorrect answer !\nThe correct answer is ' + correct + '\n')
             wrong = open('wrong_answer.txt', 'a')
-            wrong.write(ans_date + " - " + ans_hour + ' Incorrect answer question number : ' + str(page) + "\n")
+            wrong.write(ans_date + " - " + ans_hour + ' Incorrect answer question number : ' + quest1 + "\n")
             wrong.close()
             counter += 1
             clock()
@@ -115,10 +110,29 @@ def score():
             quest_number = int(c)-1
             new = open('results.txt', 'a+')
             x2 = re.split("\n\n", d2)
-            print(x2[quest_number])
             new.write("\n\n" + x2[quest_number])
             num += 1
             new.close()
+        else:
+            quit()
+
+
+def done():
+    answers = int(len(open('correct_answers.txt', 'r').readlines()))
+    wrongs = int(len(open('wrong_answer.txt', 'r').readlines()))
+    total = answers+wrongs
+    if total != 125:
+        percentage = ((answers * 100) / total)
+        finished = input(f'You have finished to exam !\nYou have {answers} right answers and {wrongs} wrong answers.\n{percentage}% of success.\nType yes to get results file').upper()
+        if finished == 'YES':
+            score()
+        else:
+            quit()
+    else:
+        percentage = ((answers * 100) / 125)
+        finished = input(f'You have finished to exam !\nYou have {answers} right answers and {wrongs} wrong answers.\n{percentage}% of success.\nType yes to get results file').upper()
+        if finished == 'YES':
+            score()
         else:
             quit()
 
@@ -127,12 +141,5 @@ while counter <= 125:
     clear()
     split()
 else:
-    answers = int(len(open('correct_answers.txt', 'r').readlines()))
-    wrongs = int(len(open('wrong_answer.txt', 'r').readlines()))
-    percentage = ((answers * 100) / 125)
-    done = input(
-        f'You have finished to exam !\nYou have {answers} right answers and {wrongs} wrong answers.\n{percentage}% of success.\nType yes to get results file').upper()
-    if done == 'YES':
-        score()
-    else:
-        quit()
+    done()
+
